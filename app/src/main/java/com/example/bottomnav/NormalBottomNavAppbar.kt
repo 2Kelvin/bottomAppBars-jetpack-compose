ba1420faa8e8c0ac5ed08a3f0b96497692236c4c
2Kelvin <rocketmancodes02@gmail.com>
2024-09-23 17:59:18 +0300

    first commit

---
+++ app/src/main/java/com/example/bottomnav/NormalBottomNavAppbar.kt
@@ -0,0 +1,9 @@
+package com.example.bottomnav
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NormalBottomNavAppbar(modifier: Modifier) {
+    //todo
+}
